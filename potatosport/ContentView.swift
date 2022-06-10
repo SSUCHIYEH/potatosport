a4bc4f8a0be1276f626fa9816bb1557575005212
rubyyeh <hyoka4654@gmail.com>
2022-06-10 10:51:24 +0800

    Initial Commit

---
+++ potatosport/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  potatosport
+//
+//  Created by ruby0926 on 2022/6/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ potatosport/potatosportApp.swift
@@ -0,0 +1,17 @@
+//
+//  potatosportApp.swift
+//  potatosport
+//
+//  Created by ruby0926 on 2022/6/10.
+//
+
+import SwiftUI
+
+@main
+struct potatosportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
