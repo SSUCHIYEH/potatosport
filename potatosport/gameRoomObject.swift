437ba00c8e882efa17a24c65b75f89e39bb517e7
rubyyeh <hyoka4654@gmail.com>
2022-06-17 17:43:39 +0800

    [feature] createRoom

---
+++ potatosport/gameRoomObject.swift
@@ -0,0 +1,28 @@
+//
+//  gameRoomObject.swift
+//  potatosport
+//
+//  Created by ruby0926 on 2022/6/12.
+//
+
+import Foundation
+
+
+
+
+//class gameRoomViewModel:Encodable,Decodable {
+//    var roomId:String = ""
+//    var mode:String = ""
+//    var fill:Bool = false
+//    var players:Int = 0
+//    var users:[userInfo]
+//}
+//
+//extension Encodable{
+//    var todictionary:[Any]? {
+//        guard let data = try? JSONEncoder().encode(self) else {
+//                return nil
+//        }
+//        return try? JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [Any]
+//    }
+//}
--- potatosport/potatosportApp.swift
+++ potatosport/potatosportApp.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 import FirebaseCore
 
+
 class AppDelegate: NSObject, UIApplicationDelegate {
   func application(_ application: UIApplication,
                    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
@@ -21,7 +22,9 @@ struct potatosportApp: App {
     @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            let viewModel = AppAuthViewModel()
+            ContentView().environmentObject(viewModel)
+            
         }
     }
 }
