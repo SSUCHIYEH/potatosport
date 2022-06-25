72db4d555a6a426f1e25c7181c7955c03480c396
TsiaYu1304 <openopen290@gmail.com>
2022-06-25 19:16:24 +0800

    fix - 以橫向開啟app

--- potatosport/ContentView.swift
+++ potatosport/ContentView.swift
@@ -10,21 +10,27 @@ import SwiftUI
 struct ContentView: View {
     @State private var isPlaying = false
     var body: some View {
+        
         if isPlaying{
             GameRunView()
            //
         }else{
             //RunActionView()
             MainView(isPlaying: self.$isPlaying)
+                .onAppear{
+                    UIDevice.current.setValue(UIInterfaceOrientation.portrait.rawValue,forKey: "orientation")
+                    AppDelegate.orientationLock = .landscapeRight
+                }
+                .onDisappear{
+                    AppDelegate.orientationLock = .all
+                }
         }
-        
-        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
-            .previewInterfaceOrientation(.landscapeLeft)
+            //.previewInterfaceOrientation(.landscapeLeft)
     }
 }
--- potatosport/potatosportApp.swift
+++ potatosport/potatosportApp.swift
@@ -9,9 +9,17 @@ import SwiftUI
 
 @main
 struct potatosportApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+class AppDelegate:NSObject,UIApplicationDelegate{
+    static var orientationLock = UIInterfaceOrientationMask.landscapeRight
+    func application(_ application: UIApplication,supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
+        return AppDelegate.orientationLock
     }
 }
